7d9ff0204b4f79ff3a3f58f5b3cb067f06a83c43
Ava Wulkan <ava.wulkan@gmail.com>
2022-01-17 11:34:52 +0100

    Removed comments

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -1,11 +1,3 @@
-// 1. Any live cell with fewer than two live neighbors dies, as if caused by
-// underpopulation.
-// 2. Any live cell with more than three live neighbors dies, as if by
-// overcrowding.
-// 3. Any live cell with two or three live neighbors lives on to the next
-// generation.
-// 4. Any dead cell with exactly three live neighbors becomes a live cell.
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
